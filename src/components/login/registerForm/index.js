7401f35d88648b9a84ab48919c87c882bc25dca9
VektorDio <67824812+VektorDio@users.noreply.github.com>
2023-04-24 22:29:53 +0300

    Added formik Rewrote login form in formik Added input validation

--- src/components/login/registerForm/index.js
+++ src/components/login/registerForm/index.js
@@ -1,34 +1,24 @@
 import React, {useState} from 'react';
 import Link from "next/link";
 import styles from "./registerForm.module.css"
+import {Form, Formik, useField} from "formik";
+import * as Yup from "yup";
+
+const MyTextInput = ({ label, ...props }) => {
+    const [field, meta] = useField(props);
+    return (
+        <div>
+            <input {...field} {...props} className={(meta.touched && meta.error) ? styles.inputInvalid : null} />
+            {meta.touched && meta.error ? (
+                <div className={styles.error}>{meta.error}</div>
+            ) : null}
+        </div>
+    );
+};
 const RegisterForm = () => {
     const [showPassword, setShowPassword] = useState(true)
-    const [passwordValidity, setPasswordValidity] = useState(true)
     function handleShowPassword(){
         setShowPassword(!showPassword)
-    }
-
-    function handlePasswordValidity(e){
-        let lowerCaseLetters = /[a-z]/g;
-        let upperCaseLetters = /[A-Z]/g;
-        let numbers = /[0-9]/g;
-        let inputValue = e.target.value
-        setPasswordValidity(false)
-        if(!inputValue.match(lowerCaseLetters)) {
-            setPasswordValidity(true)
-        }
-        // Validate capital letters
-        if(!inputValue.match(upperCaseLetters)) {
-            setPasswordValidity(true)
-        }
-        // Validate numbers
-        if(!inputValue.match(numbers)) {
-            setPasswordValidity(true)
-        }
-        // Validate length
-        if(inputValue.length < 8) {
-            setPasswordValidity(true)
-        }
     }
 
     return (
@@ -40,52 +30,94 @@ const RegisterForm = () => {
             </div>
             <div className={styles.formBody}>
                 <span className={styles.signInText}>Register your account</span>
-                <form id="stripe-login">
-
-                    <div className={styles.field}>
-                        <label htmlFor="email">Email</label>
-                        <input type="email" name="email"/>
-                    </div>
-
-                    <div className={styles.field}>
-                        <div className={styles.passwordGrid}>
-                            <label htmlFor="password">Password</label>
-                            <div className={styles.showText} onClick={handleShowPassword}>
-                                {(showPassword) ? "Show password" : "Hide password"}
-                            </div>
-                        </div>
-                        <input
-                            type={(showPassword) ? "password" : "text"}
-                            name="password"
-                            pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
-                            onChange={handlePasswordValidity}
-                        />
-                    </div>
-
-                    <div className={styles.field}>
-                        <label htmlFor="password">Confirm password</label>
-                        <input
-                            type={(showPassword) ? "password" : "text"}
-                            name="password"
-                            pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
-                        />
-                    </div>
-
-                    <div className={styles.field}>
-                        <label htmlFor="email">Organisation</label>
-                        <input type="organisation" name="organisation"/>
-                    </div>
-
-                    <div className={styles.registerButton}>
+                <Formik
+                    initialValues={{
+                        email: '',
+                        organisation: '',
+                        password: '',
+                        confirmPassword: '',
+                    }}
+                    validationSchema={Yup.object({
+                        email: Yup.string()
+                            .email('Invalid email address')
+                            .required('Required'),
+                        organisation: Yup.string()
+                            .required('Required'),
+                        password: Yup.string()
+                            .min(8, 'Password must be 8 characters long')
+                            .matches(/[0-9]/, 'Password requires a number')
+                            .matches(/[a-z]/, 'Password requires a lowercase letter')
+                            .matches(/[A-Z]/, 'Password requires an uppercase letter')
+                            .required('Required'),
+                        confirmPassword: Yup.string()
+                            .oneOf([Yup.ref('password'), null], 'Must match "Password" field value')
+                            .required('Required'),
+                    })}
+                    onSubmit={(values, { setSubmitting }) => {
+                        setTimeout(() => {
+                            alert(JSON.stringify(values, null, 2));
+                            setSubmitting(false);
+                        }, 400);
+                    }}
+                    validateOnBlur={false}
+                >{(formik) => (
+                    <Form>
                         <div className={styles.field}>
-                            <input type="submit" name="submit" value="Register" disabled={passwordValidity}/>
+                            <label htmlFor="email">Email</label>
+                            <MyTextInput
+                                label="Email Address"
+                                name="email"
+                                type="email"
+                                placeholder="jane@formik.com"
+                            />
                         </div>
-                    </div>
 
-                    <div className={styles.ssolink}>
-                        <Link href="#">Use Google instead</Link>
-                    </div>
-                </form>
+                        <div className={styles.field}>
+                            <label htmlFor="organisation">Organisation</label>
+                            <MyTextInput
+                                label="Organisation"
+                                name="organisation"
+                                type="text"
+                                placeholder="My Organisation"
+                            />
+                        </div>
+
+                        <div className={styles.field}>
+                            <div className={styles.passwordGrid}>
+                                <label htmlFor="password">Password</label>
+                                <div className={styles.showText} onClick={handleShowPassword}>
+                                    {(showPassword) ? "Show password" : "Hide password"}
+                                </div>
+                            </div>
+                            <MyTextInput
+                                label="Password"
+                                name="password"
+                                type={(showPassword) ? "password" : "text"}
+                                placeholder="********"
+                            />
+                        </div>
+
+                        <div className={styles.field}>
+                            <label htmlFor="password">Confirm password</label>
+                            <MyTextInput
+                                label="confirmPassword"
+                                name="confirmPassword"
+                                type={(showPassword) ? "password" : "text"}
+                                placeholder="********"
+                            />
+                        </div>
+
+
+                        <div className={styles.field}>
+                            <input type="submit" name="submit" value="Register" disabled={!(formik.isValid && formik.dirty)}/>
+                        </div>
+
+                        <div className={styles.ssolink}>
+                            <Link href="#">Use Google instead</Link>
+                        </div>
+                    </Form>
+                )}
+                </Formik>
             </div>
             <div className={styles.footerLink}>
                 <span className={styles.footerText}>
