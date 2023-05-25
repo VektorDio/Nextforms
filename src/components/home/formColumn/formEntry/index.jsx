801d3640076c6579ffd1a54a4c6f06854ea79d17
VektorDio <67824812+VektorDio@users.noreply.github.com>
2023-05-25 19:03:49 +0300

    Added copied to clipboard message and function

--- src/components/home/formColumn/formEntry/index.jsx
+++ src/components/home/formColumn/formEntry/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from './formEntry.module.css'
 import ActivityButton from "@/components/buttons/activityButton";
 import GenerateLinkButton from "@/components/buttons/generateLinkButton";
@@ -9,19 +9,29 @@ import Link from "next/link";
 import {useRouter} from "next/router";
 const FormEntry = ({formEntry, onActivityToggle, onDelete}) => {
     const router = useRouter()
+    const [isMessageCopied, setIsMessageCopied] = useState(false)
     function handleRedact() {
         router.push(`/home/formConstructor/redact/${formEntry.id}`)
     }
 
     function handleGenerateLink() {
+        setIsMessageCopied(true)
         navigator.clipboard.writeText(`/view/${formEntry.id}`)
     }
+
     return (
         <div className={styles.formEntry} >
             <div onClick={handleRedact} className={styles.formName}>{formEntry.name}</div>
             <div className={styles.buttons}>
                 <ActivityButton toggled={formEntry.active} onClick={() => onActivityToggle(formEntry.id, !formEntry.active)}/>
-                <GenerateLinkButton onClick={handleGenerateLink} />
+                <div className={styles.copyMessageContainer} onMouseLeave={() => setIsMessageCopied(false)}>
+                    <div className={styles.copyMessage}
+                         style={{opacity: (isMessageCopied) ? 100 : 0,
+                             visibility: (isMessageCopied) ? "visible" : "hidden"}}
+                    > Copied to clipboard </div>
+                    <GenerateLinkButton onClick={handleGenerateLink} />
+                </div>
+
                 <Link href={``}>
                     <RedactButton/>
                 </Link>
